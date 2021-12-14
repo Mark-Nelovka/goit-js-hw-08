2229c4e9b979274cc638bcdef4cc72b5db01f9c3
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2021-12-14 22:15:28 +0200

    Task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+const throttle = require("lodash.throttle");
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+    
+const _saveLocalTime = function _locTime(e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+    console.log(e.seconds)
+};
+
+let _upadateTime = throttle(_saveLocalTime, 2000)
+
+player.on('timeupdate', _upadateTime)
+
+let curTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(curTime).then(function (event) {
+    console.log(event);
+});
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
